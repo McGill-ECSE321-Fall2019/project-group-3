114dcedcb98a2a62e8931e1eb4268d4599e74629
Preyansh <preyansh98@gmail.com>
2019-10-02 23:37:22 -0400

    Started Project!

---
+++ src/main/java/com/ecse321/group3/tutorME/TutorMeApplication.java
@@ -0,0 +1,13 @@
+package com.ecse321.group3.tutorME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorMeApplication.class, args);
+	}
+
+}
