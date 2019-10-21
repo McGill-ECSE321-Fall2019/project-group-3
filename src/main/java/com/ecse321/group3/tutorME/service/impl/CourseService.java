9734a0b9f229518ab4c9a4b3b05ce15cc5e34097
NicolasAbdelnour <nicolasabdelnour167@gmail.com>
2019-10-21 16:38:37 -0400

    add createCourse and getCourse methods

---
+++ src/main/java/com/ecse321/group3/tutorME/service/impl/CourseService.java
@@ -0,0 +1,64 @@
+package com.ecse321.group3.tutorME.service.impl;
+
+import com.ecse321.group3.tutorME.domain.Course;
+import com.ecse321.group3.tutorME.domain.Subject;
+import com.ecse321.group3.tutorME.domain.University;
+import com.ecse321.group3.tutorME.repository.CourseRepository;
+import com.ecse321.group3.tutorME.service.CourseServiceIF;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Service
+public class CourseService implements CourseServiceIF {
+
+    @Autowired
+    private CourseRepository courseRepo;
+
+    @Override
+    public Course createCourse(Course course) throws Exception {
+        //THIS METHOD WILL DO THE ACTUAL CODE IMPLEMENTATION.
+        Course courseCreated = null;
+
+        //create the course, by saving to the database.
+        try {
+            courseCreated = courseRepo.save(course);
+        } catch(Exception e){
+            //if we get errors saving to database, throw an exception
+            throw new Exception(e.getMessage());
+        }
+
+        //or else return the course we created.
+        return courseCreated;
+    }
+
+    @Override
+    public Course getCourse(String courseName) throws Exception {
+        Course courseCreated = null;
+
+        //create the course, by saving to the database.
+        try {
+            courseCreated = courseRepo.findById(courseName).get();
+        } catch(Exception e){
+            //if we get errors getting to database, throw an exception
+            throw new Exception(e.getMessage());
+        }
+
+        //or else return the course we created.
+        return courseCreated;    
+        }
+
+    @Override
+    public List<Course> getCourses() throws Exception {
+        return null;
+    }
+
+    @Override
+    public void deleteCourse(String courseName) throws Exception {
+
+    }
+
+
+}
