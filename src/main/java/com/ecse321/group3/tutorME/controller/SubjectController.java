0bb17616dfbad07d7fdcc076e74ca61ac1325c27
Preyansh <preyansh98@gmail.com>
2019-10-21 14:50:29 -0400

    Fix Build

--- src/main/java/com/ecse321/group3/tutorME/controller/SubjectController.java
+++ src/main/java/com/ecse321/group3/tutorME/controller/SubjectController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Response;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
