f42b165e645862d93911ddf068381d428c3da5ac
Karl Eid <karl.eid@mail.mcgill.ca>
2019-10-22 15:37:05 -0400

    Added Create lesson,Get lesson by id, Get a list of all lessons, Delete lesson (only if start time is later than today
    Still have to fix a minor issue in create lesson where the id created is 1 instead of 0 when I explicity say that it 0, as well as add the update methods

---
+++ src/main/java/com/ecse321/group3/tutorME/service/impl/LessonService.java
@@ -0,0 +1,84 @@
+package com.ecse321.group3.tutorME.service.impl;
+
+import com.ecse321.group3.tutorME.domain.Lesson;
+import com.ecse321.group3.tutorME.domain.Subject;
+import com.ecse321.group3.tutorME.domain.University;
+import com.ecse321.group3.tutorME.repository.LessonRepository;
+import com.ecse321.group3.tutorME.service.LessonServiceIF;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class LessonService implements LessonServiceIF {
+
+    @Autowired
+    private LessonRepository lessonRepo;
+
+    @Override
+    public Lesson createLesson(Lesson lesson) throws Exception {
+        //THIS METHOD WILL DO THE ACTUAL CODE IMPLEMENTATION.
+        Lesson lessonCreated = null;
+
+        //create the course, by saving to the database.
+        try {
+        	lessonCreated = lessonRepo.save(lesson);
+        } catch(Exception e){
+            //if we get errors saving to database, throw an exception
+            throw new Exception(e.getMessage());
+        }
+
+        //or else return the course we created.
+        return lessonCreated;
+    }
+
+    @Override
+    public Lesson getLesson(int lessonId) throws Exception {
+        Lesson lessonCreated = null;
+
+        //create the course, by saving to the database.
+        try {
+            lessonCreated = lessonRepo.findById(lessonId).get();
+        } catch(Exception e){
+            //if we get errors getting to database, throw an exception
+            throw new Exception(e.getMessage());
+        }
+
+        //or else return the course we created.
+        return lessonCreated;    
+        }
+
+    @Override
+    public List<Lesson> getLessons() throws Exception {
+        List<Lesson> lessons = null;
+
+        //create the course, by saving to the database.
+        try {
+            lessons = lessonRepo.findAll();
+        } catch(Exception e){
+            //if we get errors getting to database, throw an exception
+            throw new Exception(e.getMessage());
+        }
+
+        //or else return the course we created.
+        return lessons;     
+        }
+
+    @Override
+    public void deleteLesson(int lessonId) throws Exception {
+        //delete the course
+        try {
+            lessonRepo.deleteById(lessonId);
+        } catch(Exception e){
+            //if we get errors getting to database, throw an exception
+            throw new Exception(e.getMessage());
+        }
+        return; 
+    }
+
+
+}
