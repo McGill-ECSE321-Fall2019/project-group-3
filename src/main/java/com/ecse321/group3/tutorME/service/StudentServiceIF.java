f06b03cf6741e67f21eb28590c381e5d35da75f5
Elie-Elia <elie.elia@mail.mcgill.ca>
2019-10-23 20:17:03 -0400

    Student controller

---
+++ src/main/java/com/ecse321/group3/tutorME/service/StudentServiceIF.java
@@ -0,0 +1,18 @@
+package com.ecse321.group3.tutorME.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ecse321.group3.tutorME.domain.Student;
+@Service
+public interface StudentServiceIF {
+	
+	Student createStudent(Student student) throws Exception;
+    List<Student> getStudents() throws Exception;
+    //todo: update.
+    void deleteStudent(String emailAddress) throws Exception;
+	Student getStudent(String emailAddress);
+	Student updateStudent(int oldId, Student student);
+
+}
