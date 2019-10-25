3c01bb1f9110c6837c03b04ada124768154fa0d3
Elie-Elia <elie.elia@mail.mcgill.ca>
2019-10-24 21:03:33 -0400

    UniversityTest

--- src/test/java/com/ecse321/group3/tutorME/domain/StudentTest.java
+++ src/test/java/com/ecse321/group3/tutorME/domain/StudentTest.java
@@ -1,5 +1,6 @@
 package com.ecse321.group3.tutorME.domain;
 
+import com.ecse321.group3.tutorME.repository.UserEntityRepository;
 import com.ecse321.group3.tutorME.repository.UserRoleRepository;
 import com.ecse321.group3.tutorME.service.StudentServiceIF;
 
@@ -24,6 +25,13 @@ public class StudentTest {
 	
 	@Autowired
     private UserRoleRepository userRoleRepo;
+	@Autowired
+    private UserEntityRepository userEntityRepository;
+
+	
+	
+	UserEntity userForStudent = null;
+	String emailForUser = "testemail@test.com";
 
 	@Autowired
     private TestSuiteUtils testUtils;
@@ -37,59 +45,70 @@ public class StudentTest {
     @Transactional
     public void createStudent(){
 		Student studentEntity = new Student();
-		studentEntity.getUser().setEmail("hello");
+		studentEntity.setUserId(100);
+		userForStudent = new UserEntity();
+        userForStudent.setEmail(emailForUser);
+		userForStudent.setUserRole(studentEntity);
 
-        try {
+		studentEntity.setUser(userForStudent);
+
+
+		try{
             studentService.createStudent(studentEntity);
         } catch(Exception e){
             Assert.fail(e.getMessage());
         }
     }
 	
-	@Test
-    @Transactional
-    public void getStudent() throws Exception {
-        createStudent();
-        Assert.assertEquals(1, userRoleRepo.findAll().size());
-    }
+	 @Test
+	    @Transactional
+	    public void getStudent() throws Exception {
+	        Student student = new Student();
+	        student.setUserId(100);
 
-	@Test
-	@Transactional
-	public void updateStudent() throws Exception {
-		createStudent();
+	        userForStudent = new UserEntity();
+	        userForStudent.setEmail(emailForUser);
+	        userForStudent.setUserRole(student);
 
-		Student newStudent = new Student();
-		newStudent.getUser().setEmail("hello2");
+	        student.setUser(userForStudent);
+	        userEntityRepository.save(userForStudent);
+	        Assert.assertEquals(emailForUser, studentService.getStudent(emailForUser).getUser().getEmail());
+	    }
 
-		try {
-			studentService.updateStudent("hello", newStudent);
-		} catch (Exception e) {
-			Assert.fail(e.getMessage());
+	    @Test
+	    @Transactional
+	    public void updateStudent() throws Exception{
+		    createStudent();
+		    Student newStudent = new Student();
+		    newStudent.setUserId(121);
+
+		    Assert.assertEquals(1, studentService.getStudents().size());
+
+		    studentService.updateStudent(emailForUser, newStudent);
+
+		    Assert.assertEquals(1, studentService.getStudents().size());
+	    }
+
+	    @Test
+	    @Transactional
+	    public void getAllStudents() throws Exception{
+	        createStudent();
+	        Assert.assertEquals(1, studentService.getStudents().size());
+	    }
+
+	    @Test
+	    @Transactional
+	    public void deleteStudent() throws Exception{
+		    UserEntity createUser = new UserEntity();
+		    createUser.setEmail(emailForUser);
+		    Student student = new Student();
+		    createUser.setUserRole(student);
+	        student.setUser(createUser);
+	        studentService.createStudent(student);
+		    userEntityRepository.save(createUser);
+
+		    userRoleRepo.deleteById(studentService.getStudents().get(0).getUserId());
+		    Assert.assertEquals(0, studentService.getStudents().size());
+	    }
+
 		}
-
-		try {
-			studentService.getStudent("hello");
-		} catch(Exception e){
-		}
-		Assert.assertEquals("hello2", studentService.getStudent("hello2").getUser().getEmail());
-	}
-	
-	@Test
-	@Transactional
-	public void getAllStudents() throws Exception{
-		createStudent();
-		Student newStudent = new Student();
-		newStudent.getUser().setEmail("hello3");
-		studentService.createStudent(newStudent);
-
-		Assert.assertEquals(2, studentService.getStudents().size());
-	}
-
-	@Test
-	@Transactional
-	public void deleteStudent() throws Exception{
-		createStudent();
-		studentService.deleteStudent("hello");
-		Assert.assertEquals(0, studentService.getStudents().size());
-	}
-}
