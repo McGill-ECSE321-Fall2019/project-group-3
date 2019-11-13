a84a78120e39d7d6b2a484aab94898c61346907e
Elie-Elia <elie.elia@mail.mcgill.ca>
2019-11-12 23:30:08 -0500

    Update Welcome.vue

--- tutorMe-frontend/src/components/Welcome.vue
+++ tutorMe-frontend/src/components/Welcome.vue
@@ -1,59 +1,5 @@
 <template>
   <div>
-      <center>
-    <b-form @submit="onSubmit" class="mainForm">
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          required
-          placeholder="Enter email"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-form-group id="input-group-2" label="Your Password:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.password"
-          type="password"
-          required
-          placeholder="Enter password"
-        ></b-form-input>
-      </b-form-group>
-
-      <b-button type="submit" variant="primary">Login!</b-button>
-      <b-button variant="primary">Or Sign Up!</b-button>
-    </b-form>
-    </center>
-  </div>
-</template>
-
-<script>
-  export default {
-    data() {
-      return {
-        form: {
-          email: '',
-          password: ''
-        }
-      }
-    },
-    methods: {
-      onSubmit(evt) {
-        evt.preventDefault()
-        alert(JSON.stringify(this.form))
-      }
-    }
-  }
-</script>
-<template>
-  <div>
     <b-carousel
       id="carousel-1"
       v-model="slide"
@@ -130,6 +76,62 @@
     }
   }
 </script>
+
+<template>
+  <div>
+      <center>
+    <b-form @submit="onSubmit" class="mainForm">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Password:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          type="password"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Login!</b-button>
+      <b-button variant="primary">Or Sign Up!</b-button>
+    </b-form>
+    </center>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          email: '',
+          password: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      }
+    }
+  }
+</script>
+
 
 <style scoped>
 .mainForm{
