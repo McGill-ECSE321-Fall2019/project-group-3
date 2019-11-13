6b844e157371aa152f43c29a7da0f12e179e13b4
Preyansh <preyansh98@gmail.com>
2019-11-12 23:04:52 -0500

    Created all components

---
+++ tutorMe-frontend/src/components/Lesson.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <b-card class="text-center">
+    <div class="bg-secondary text-light">
+      This is some content within the default card.
+    </div>
+  </b-card>
+
+   <b-list-group-item href="#" class="flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">List group item heading</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+
+    <p class="mb-1">
+      Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.
+    </p>
+
+    <small class="text-muted">Donec id elit non mi porta.</small>
+  </b-list-group-item>
+</div>
+</template>
