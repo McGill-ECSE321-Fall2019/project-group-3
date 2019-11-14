dc25d8a117632099e59b5ece3328be19f5a4efbc
jasondaou <jason.daou@gmail.com>
2019-11-13 19:49:23 -0500

    Update tutors page front end

--- tutorMe-frontend/src/components/Tutors.vue
+++ tutorMe-frontend/src/components/Tutors.vue
@@ -3,26 +3,23 @@
     <br>
     <br>
 <div class = "mainForm">
-  <b-card-group deck>
-  <b-card header="Card with list group">
+  <b-card-group deck v-for="index in 10" :key="index">
+  <b-card header="Tutor name" >
     <b-list-group>
-      <b-list-group-item href="#" class="flex-column align-items-start" v-for="index in 10" :key="index">
+      <b-list-group-item href="#" class="flex-column align-items-start" v-for="index in 3" :key="index">
     <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">Tutor Name: tutor</h5>
+      <h5 class="mb-1">Example course</h5>
     </div>
 
     <p class="mb-1">
-      Some attributes to display for tutor. 
+      Course information. 
     </p>
 
-    <small class="text-muted">Click to update the tutor.</small>
     </b-list-group-item>
     </b-list-group>
 
     <p class="card-text mt-2">
-      Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla tempor. Laborum
-      consequat non elit enim exercitation cillum aliqua consequat id aliqua. Esse ex consectetur
-      mollit voluptate est in duis laboris ad sit ipsum anim Lorem.
+      Review summary
     </p>
   </b-card>
   </b-card-group>
