b032ceb7c764ec7158adaeee730d702a21b39087
Elie-Elia <elie.elia@mail.mcgill.ca>
2019-11-14 15:34:41 -0500

    Removed unnecessary import

--- tutorMe-frontend/src/router/index.js
+++ tutorMe-frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import Lesson from '@/components/Lesson'
 import Welcome from '@/components/Welcome'
 import Course from '@/components/Course'
