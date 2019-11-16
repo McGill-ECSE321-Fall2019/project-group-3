5d8df4c72d212fbe3abd88ec182cbb8aabb1f368
Elie-Elia <elie.elia@mail.mcgill.ca>
2019-11-16 15:52:10 -0500

    Update CreateRoom.vue

--- tutorMe-frontend/src/components/Room/CreateRoom.vue
+++ tutorMe-frontend/src/components/Room/CreateRoom.vue
@@ -19,17 +19,10 @@
                 label="Select Room size:"
                 label-for="input-2"
               >
-                <b-form-input
-                  id="input-2"
-                  input="dropdown-1">
+              <b-form-select :v-model="size_selected" :options="size_options"></b-form-select>
+       </b-form-group>
 
-                  
-                  </b-form-input>
-                  <b-form-select :v-model="size_selected" :options="size_options"></b-form-select>
-        
-              </b-form-group>
-
-              <b-form-group
+        <b-form-group
                 id="input-group-3"
                 label="Choose room starting availability:"
                 label-for="input-3"
@@ -40,6 +33,22 @@
                     input-class="form-control"
                     name="start"
                     label="Please enter lesson start time"
+                    v-model="form.startTime"
+                  ></VueCtkDateTimePicker>
+                </div>
+              </b-form-group>
+
+              <b-form-group
+                id="input-group-3"
+                label="Choose room ending availability:"
+                label-for="input-3"
+              >
+                <div>
+                  <VueCtkDateTimePicker
+                    :no-value-to-custom-elem="(true|false)"
+                    input-class="form-control"
+                    name="start"
+                    label="Please enter Room availability ending time"
                     v-model="form.startTime"
                   ></VueCtkDateTimePicker>
                 </div>
