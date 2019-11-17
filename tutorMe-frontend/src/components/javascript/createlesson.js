c7c990203ea43131b2bfd19720a1af39ac6c391f
Karl Eid <karl.eid@mail.mcgill.ca>
2019-11-16 20:27:12 -0500

    Student now shows first name, last name, email and reviews. Still need to fix reviews

--- tutorMe-frontend/src/components/javascript/createlesson.js
+++ tutorMe-frontend/src/components/javascript/createlesson.js
@@ -52,7 +52,7 @@ export default {
                     }
                 });
             }));
-            console.dir(self.roomOps); 
+            console.dir(self.roomOps);
     },
     methods: {
         onSubmit(evt) {
